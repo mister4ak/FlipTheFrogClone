6c804e2d8983b7a5b582c75e36b462e3e6f46fe9
mister4ak <mister4ak@gmail.com>
2022-05-03 14:54:40 +0300

    - добавил 7 уровень - добавил новую input system (нужна переработка)

--- Assets/CodeBase/Infrastructure/Services/Input/MouseInput.cs
+++ Assets/CodeBase/Infrastructure/Services/Input/MouseInput.cs
@@ -1,29 +1,72 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using Zenject;
 
 namespace CodeBase.Infrastructure.Services.Input
 {
-    public class MouseInput : IInputService, ITickable
+    public class MouseInput : IInputService, IInitializable
     {
         private Vector2 _inputPosition;
         public event Action<Vector2> Clicked;
         public event Action<Vector2> Hold;
         public event Action<Vector2> Released;
-        
-        
-        public void Tick()
+
+        private PlayerInput _inputMap;
+        private InputAction _position;
+
+        public void Initialize()
         {
-            _inputPosition = UnityEngine.Input.mousePosition;
+            _inputMap = new PlayerInput();
+            _inputMap.Enable();
 
-            if (UnityEngine.Input.GetMouseButtonDown(0)) 
-                Clicked?.Invoke(_inputPosition);
+            //_position = _inputMap.Player.Position;
+            
+            _inputMap.Player.Click.started += OnClicked;
+            _inputMap.Player.Click.performed += OnHold;
+            _inputMap.Player.Click.canceled += OnReleased;
+            
+            //_inputMap.Player.Click
+        }
 
-            if (UnityEngine.Input.GetMouseButton(0))
-                Hold?.Invoke(_inputPosition);
+        private void OnReleased(InputAction.CallbackContext obj)
+        {
+            Debug.Log("released");
+            Released?.Invoke(obj.ReadValue<Vector2>());
+        }
 
-            if (UnityEngine.Input.GetMouseButtonUp(0)) 
-                Released?.Invoke(_inputPosition);
+        private void OnHold(InputAction.CallbackContext obj)
+        {
+            Debug.Log("hold");
+            Hold?.Invoke(obj.ReadValue<Vector2>());
+        }
+
+        private void OnClicked(InputAction.CallbackContext context)
+        {
+            Debug.Log("clicked");
+            Clicked?.Invoke(context.ReadValue<Vector2>());
         }
     }
+    // public class MouseInput : IInputService, ITickable
+    // {
+    //     private Vector2 _inputPosition;
+    //     public event Action<Vector2> Clicked;
+    //     public event Action<Vector2> Hold;
+    //     public event Action<Vector2> Released;
+    //     
+    //     
+    //     public void Tick()
+    //     {
+    //         _inputPosition = UnityEngine.Input.mousePosition;
+    //
+    //         if (UnityEngine.Input.GetMouseButtonDown(0)) 
+    //             Clicked?.Invoke(_inputPosition);
+    //
+    //         if (UnityEngine.Input.GetMouseButton(0))
+    //             Hold?.Invoke(_inputPosition);
+    //
+    //         if (UnityEngine.Input.GetMouseButtonUp(0)) 
+    //             Released?.Invoke(_inputPosition);
+    //     }
+    // }
 }
---
+++ Assets/CodeBase/Infrastructure/Services/Input/PlayerInput.cs
@@ -0,0 +1,161 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/CodeBase/Infrastructure/Services/Input/PlayerInput.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerInput : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerInput()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerInput"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""62cb1201-9d84-4f68-874e-bfcd7d92aa5a"",
+            ""actions"": [
+                {
+                    ""name"": ""Click"",
+                    ""type"": ""Button"",
+                    ""id"": ""950cc423-8060-4c1c-adc4-4425fc942c95"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Position"",
+                    ""type"": ""Value"",
+                    ""id"": ""89a56520-ce65-46f2-b288-1b7a9ab5131c"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""799d1937-1405-4740-9822-977a3b1a1855"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Click"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""93e21f14-9b65-4077-9b58-284586c5d8f8"",
+                    ""path"": ""<Mouse>/position"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Position"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Click = m_Player.FindAction("Click", throwIfNotFound: true);
+        m_Player_Position = m_Player.FindAction("Position", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Click;
+    private readonly InputAction m_Player_Position;
+    public struct PlayerActions
+    {
+        private @PlayerInput m_Wrapper;
+        public PlayerActions(@PlayerInput wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Click => m_Wrapper.m_Player_Click;
+        public InputAction @Position => m_Wrapper.m_Player_Position;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Click.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnClick;
+                @Click.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnClick;
+                @Click.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnClick;
+                @Position.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPosition;
+                @Position.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPosition;
+                @Position.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnPosition;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Click.started += instance.OnClick;
+                @Click.performed += instance.OnClick;
+                @Click.canceled += instance.OnClick;
+                @Position.started += instance.OnPosition;
+                @Position.performed += instance.OnPosition;
+                @Position.canceled += instance.OnPosition;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnClick(InputAction.CallbackContext context);
+        void OnPosition(InputAction.CallbackContext context);
+    }
+}
